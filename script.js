80ec71831bef97ff64cf3d9112aa695065fcb17e
SRINIDHI <srinidhivs28@gmail.com>
2023-02-03 14:56:23 +0530

    Added logic for the search icon

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-let menu  = document.querySelector('#menu-bars');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
 
-menu.addEventListener('click',()=>
-{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+menu.addEventListener("click", () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+});
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
+
+document.querySelector("#search-icon").addEventListener("click", () => {
+  document.querySelector("#search-form").classList.toggle("active");
+});
+
+document.querySelector("#close").addEventListener("click", () => {
+  document.querySelector("#search-form").classList.remove("active");
 });
