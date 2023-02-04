1cea0aa6360dc30509d95bfd6cd2d0a1a250bcba
SRINIDHI <srinidhivs28@gmail.com>
2023-02-04 17:59:39 +0530

    Added Dishes, About, Menu and Review section

--- script.js
+++ script.js
@@ -18,3 +18,45 @@ document.querySelector("#search-icon").addEventListener("click", () => {
 document.querySelector("#close").addEventListener("click", () => {
   document.querySelector("#search-form").classList.remove("active");
 });
+
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 47,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
+
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 138,
+  centeredSlides: true,
+  autoplay: {
+    delay: 6500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+breakpoints:
+{
+  0:{
+    slidesPerView :1,
+  },
+  640:{
+    slidesPerView :2,
+  },
+  768:
+  {
+    slidesPerView :2,
+  },
+  1024:
+  {
+    slidesPerView :3,
+  },
+},
+});
